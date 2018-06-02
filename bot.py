0294853d80cc5bee7c5649e4c216e03e0cb873ae
flacidUnicorn4 <39836596+flacidUnicorn4@users.noreply.github.com>
2018-06-02 04:22:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,10 @@ from discord.ext import commands
 
 bot = commands.Bot(description="Hey! I'm Fruitbowl's personal bot! Use me at any time by using ! ", command_prefix="!")
 
-time = datetime.datetime.utcnow()
-
+t= datetime.datetime.utcnow()
 @bot.event
 async def on_ready():
- await bot.change_presence(activity= discord.Streaming(name="help via !help", url="https://.twitch.tv/blablabla"))
+ await bot.change_presence (activity= discord.Streaming(name="help via !help", url="www.twitch.tv/blablabla"))
 
 
 
@@ -18,8 +17,8 @@ async def time(ctx):
     "> This command tells you the time in GMT"
 
     embed = discord.Embed(title="GMT Date and time", description=" ")
-    embed.add_field(name="Date", value=f"{time.date()}")
-    embed.add_field(name="time", value=f"{time.time().strftime('%H:%M:%S')}")
+    embed.add_field(name="Date", value=f"{t.date()}")
+    embed.add_field(name="time", value=f"{t.time().strftime('%H:%M:%S')}")
     await ctx.send(embed=embed)
 
 
